72ced035ba639447c8608373b6eb461bbbfa5436
Spencehiko <hikmet.kufteoglu@gmail.com>
2022-02-23 22:33:04 +0300

    fix! channel page

---
+++ src/components/Channel/ChannelInfo.vue
@@ -0,0 +1,238 @@
+<template>
+    <div class="channels" :class="{ fullPage: !showSidemenu }">
+        <div
+            class="channel-banner"
+            :style="{
+                background:
+                    'center / cover no-repeat url(' +
+                    channelInfo.bannerSrc +
+                    ')',
+            }"
+        ></div>
+        <div class="channel-header">
+            <img :src="channelInfo.imgSrc" :alt="channelInfo.name" />
+            <div class="channel-info">
+                <div class="data">
+                    <div class="name">
+                        <span>{{ channelInfo.name }}</span>
+                        <svg
+                            v-if="channelInfo.verified"
+                            viewBox="0 0 24 24"
+                            preserveAspectRatio="xMidYMid meet"
+                            focusable="false"
+                        >
+                            <g>
+                                <path
+                                    d="M12,2C6.5,2,2,6.5,2,12c0,5.5,4.5,10,10,10s10-4.5,10-10C22,6.5,17.5,2,12,2z M9.8,17.3l-4.2-4.1L7,11.8l2.8,2.7L17,7.4 l1.4,1.4L9.8,17.3z"
+                                ></path>
+                            </g>
+                        </svg>
+                    </div>
+                    <span class="subs">{{
+                        parseSubs(channelInfo.subscribers) + " subscribers"
+                    }}</span>
+                </div>
+                <div class="buttons">
+                    <button class="join">JOIN</button>
+                    <button class="subscribe">SUBSCRIBED</button>
+                    <svg
+                        viewBox="0 0 24 24"
+                        preserveAspectRatio="xMidYMid meet"
+                        focusable="false"
+                    >
+                        <g>
+                            <path
+                                d="M10,20h4c0,1.1-0.9,2-2,2S10,21.1,10,20z M20,17.35V19H4v-1.65l2-1.88v-5.15c0-2.92,1.56-5.22,4-5.98V3.96 c0-1.42,1.49-2.5,2.99-1.76C13.64,2.52,14,3.23,14,3.96l0,0.39c2.44,0.75,4,3.06,4,5.98v5.15L20,17.35z M19,17.77l-2-1.88v-5.47 c0-2.47-1.19-4.36-3.13-5.1c-1.26-0.53-2.64-0.5-3.84,0.03C8.15,6.11,7,7.99,7,10.42v5.47l-2,1.88V18h14V17.77z"
+                            ></path>
+                        </g>
+                    </svg>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import store from '@/store';
+import videos from '@/datas/videos.json';
+import channels from '@/datas/channels.json';
+
+export interface Video {
+    id: number,
+    title: string,
+    channelId: number,
+    imgSrc: string,
+    date: string,
+    views: number,
+    likes: number,
+    dislikes: number,
+    duration: number
+}
+export interface Channel {
+    id: number,
+    name: string,
+    imgSrc: string,
+    bannerSrc: string,
+    subscribers: number,
+    verified: boolean
+}
+
+export default defineComponent({
+    name: 'channelInfo',
+    data() {
+        return {
+            channels: channels.channels as Channel[],
+            videos: videos.videos as Video[],
+            showSidemenu: true,
+        }
+    },
+    computed: {
+        channelInfo(): Channel {
+            return this.channels.filter((channel: Channel) => channel.id.toString() === this.channelId)[0]
+        },
+        channelId() {
+            return this.$route.query.id?.toString()
+        },
+    },
+    methods: {
+        convertTime(duration: number) {
+            const hours = Math.floor(duration / 3600);
+            const durationMod = duration %= 3600;
+            const minutes = Math.floor(durationMod / 60);
+            const seconds = durationMod % 60;
+            return (hours > 0 ? hours + ':' : '') + ((hours > 0 && minutes < 10) ? '0' + minutes : minutes) + ':' + (seconds < 10 ? '0' + seconds : seconds)
+        },
+        parseSubs(subs: number) {
+            if (subs <= 999) {
+                return subs;
+            }
+            if (subs <= 9999) {
+                const k = Math.floor(subs / 1000);
+                const remainder = Math.round(subs % 1000 / 100);
+                return k + '.' + remainder + 'K'
+            }
+            if (subs <= 999999) {
+                const k = Math.floor(subs / 1000);
+                return k + 'K'
+            }
+            return Math.floor(subs / 1000) + '.' + Math.round(subs % 1000 / 100) + 'M';
+        },
+        getDateDifference(date: string) {
+            const today = new Date().valueOf();
+            const dateTime = new Date(date).valueOf();
+            const diffTime = today - dateTime;
+            const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+            if (diffDays < 1) {
+                return '9 hours ago';
+            }
+            if (diffDays < 7) {
+                return diffDays - 1 + (diffDays === 1 ? ' day ago' : ' days ago');
+            }
+            if (diffDays < 31) {
+                return Math.floor(diffDays / 7) + (Math.floor(diffDays / 7) === 1 ? ' week ago' : ' weeks ago')
+            }
+            if (diffDays < 365) {
+                return Math.floor(diffDays / 31) + (Math.floor(diffDays / 31) === 1 ? ' month ago' : ' months ago')
+            }
+            if (diffDays >= 365) {
+                return Math.floor(diffDays / 365) + (Math.floor(diffDays / 365) === 1 ? ' year ago' : ' years ago')
+            }
+        }
+    },
+    beforeMount() {
+        this.showSidemenu = store.getters.showSidemenu
+        store.watch((getters) => getters.showSidemenu, () => {
+            this.showSidemenu = store.getters.showSidemenu;
+        })
+    },
+});
+</script>
+
+<style lang="less" scoped>
+.channels {
+    margin-left: 240px;
+    margin-top: 56px;
+    background: #181818;
+    .channel-banner {
+        width: 100%;
+        height: calc((100vw - 240px) / 6);
+        background-size: contain;
+    }
+    .channel-header {
+        padding: 16px calc((100% - 1284px) / 2) 4px;
+        display: flex;
+        img {
+            width: 80px;
+            height: 80px;
+            margin: 0 24px 0 0;
+            flex: none;
+            border-radius: 50%;
+            background-color: transparent;
+            overflow: hidden;
+        }
+        .channel-info {
+            flex-wrap: wrap;
+            flex: 1;
+            flex-basis: 1e-9px;
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            .data {
+                min-width: 150px;
+                flex: 1;
+                flex-basis: 1e-9px;
+                .name {
+                    display: inline-flex;
+                    width: 100%;
+                    span {
+                        font-size: 24px;
+                        font-weight: 400;
+                    }
+                    svg {
+                        height: 14px;
+                        width: 14px;
+                        margin: auto 5px;
+                    }
+                }
+                .subs {
+                    color: #aaa;
+                    font-size: 14px;
+                    font-weight: 400;
+                }
+            }
+            .buttons {
+                display: flex;
+                flex-basis: 20%;
+                justify-content: space-between;
+                button {
+                    background: transparent;
+                    color: #3ea6ff;
+                    height: 37px;
+                    padding: 9px 15px;
+                    border: 1px solid #3ea6ff;
+                    border-radius: inherit;
+                    font-weight: 500;
+                    font-size: 14px;
+                    font-family: "Roboto", "Noto", sans-serif;
+                    &.subscribe {
+                        border: none;
+                        background: rgba(255, 255, 255, 0.1);
+                        color: #aaa;
+                    }
+                }
+                svg {
+                    margin: auto 0;
+                }
+            }
+        }
+    }
+}
+@media screen and (max-width: 1328px) {
+    .channels {
+        .channel-banner {
+            height: calc(16.2vw - 1px);
+        }
+    }
+}
+</style>
---
+++ src/components/Channel/Videos.vue
@@ -0,0 +1,339 @@
+<template>
+    <div class="videos" :class="{ fullPage: !showSidemenu }">
+        <router-link
+            class="video"
+            v-for="video in channelVideos"
+            :key="video.id"
+            :to="'/video?v=' + video.id"
+        >
+            <div class="thumbnail">
+                <img :src="video.imgSrc" :alt="video.title" />
+                <div class="duration">
+                    {{ convertTime(video.duration) }}
+                </div>
+            </div>
+            <div class="video-data">
+                <div class="video-info">
+                    <img
+                        :src="getChannelPP(video.channelId)"
+                        :title="getChannelName(video.channelId)"
+                        class="video-channel-pp"
+                    />
+                    <div class="video-metadata">
+                        <span class="video-title" :title="video.title">
+                            {{ video.title }}
+                        </span>
+                        <div class="video-channel-data">
+                            <router-link
+                                class="video-channel-name"
+                                :to="'/channel?id=' + video.channelId"
+                            >
+                                {{ getChannelName(video.channelId) }}
+                            </router-link>
+                            <svg
+                                v-if="getChannelVerified(video.channelId)"
+                                viewBox="0 0 24 24"
+                                preserveAspectRatio="xMidYMid meet"
+                                focusable="false"
+                            >
+                                <g>
+                                    <path
+                                        d="M12,2C6.5,2,2,6.5,2,12c0,5.5,4.5,10,10,10s10-4.5,10-10C22,6.5,17.5,2,12,2z M9.8,17.3l-4.2-4.1L7,11.8l2.8,2.7L17,7.4 l1.4,1.4L9.8,17.3z"
+                                    ></path>
+                                </g>
+                            </svg>
+                        </div>
+                        <div class="video-statistics">
+                            <span class="video-views">
+                                {{ parseViews(video.views) + " views" }}
+                            </span>
+                            <span class="video-date">
+                                {{ getDateDifference(video.date) }}
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="video-options">
+                    <svg
+                        viewBox="0 0 24 24"
+                        preserveAspectRatio="xMidYMid meet"
+                        focusable="false"
+                    >
+                        <g>
+                            <path
+                                d="M12,16.5c0.83,0,1.5,0.67,1.5,1.5s-0.67,1.5-1.5,1.5s-1.5-0.67-1.5-1.5S11.17,16.5,12,16.5z M10.5,12 c0,0.83,0.67,1.5,1.5,1.5s1.5-0.67,1.5-1.5s-0.67-1.5-1.5-1.5S10.5,11.17,10.5,12z M10.5,6c0,0.83,0.67,1.5,1.5,1.5 s1.5-0.67,1.5-1.5S12.83,4.5,12,4.5S10.5,5.17,10.5,6z"
+                            ></path>
+                        </g>
+                    </svg>
+                </div>
+            </div>
+        </router-link>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import store from '@/store';
+import videos from '@/datas/videos.json';
+import channels from '@/datas/channels.json';
+
+export interface Video {
+    id: number,
+    title: string,
+    channelId: number,
+    imgSrc: string,
+    date: string,
+    views: number,
+    likes: number,
+    dislikes: number,
+    duration: number
+}
+export interface Channel {
+    id: number,
+    name: string,
+    imgSrc: string,
+    subscribers: number,
+    verified: boolean
+}
+
+export default defineComponent({
+    name: 'Videos',
+    data() {
+        return {
+            videos: videos.videos as Video[],
+            channels: channels.channels as Channel[],
+            showSidemenu: true,
+        }
+    },
+    computed: {
+        shuffle(array: Array<Video>) {
+            let currentIndex = array.length, randomIndex;
+            while (currentIndex != 0) {
+                randomIndex = Math.floor(Math.random() * currentIndex);
+                currentIndex--;
+                [array[currentIndex], array[randomIndex]] = [
+                    array[randomIndex], array[currentIndex]];
+            }
+            return array;
+        },
+        channelVideos(): any {
+            return this.videos.filter((video: Video) => video.channelId.toString() === this.channelId)
+        },
+        channelId() {
+            return this.$route.query.id?.toString()
+        },
+    },
+    methods: {
+        getChannelPP(channelId: number): string {
+            return this.channels.filter((channel) => channel.id === channelId)[0].imgSrc;
+        },
+        getChannelName(channelId: number): string {
+            return this.channels.filter((channel) => channel.id === channelId)[0].name;
+        },
+        getChannelVerified(channelId: number): boolean {
+            return this.channels.filter((channel) => channel.id === channelId)[0].verified;
+        },
+        convertTime(duration: number) {
+            const hours = Math.floor(duration / 3600);
+            const durationMod = duration %= 3600;
+            const minutes = Math.floor(durationMod / 60);
+            const seconds = durationMod % 60;
+            return (hours > 0 ? hours + ':' : '') + ((hours > 0 && minutes < 10) ? '0' + minutes : minutes) + ':' + (seconds < 10 ? '0' + seconds : seconds)
+        },
+        parseViews(views: number) {
+            if (views <= 999) {
+                return views;
+            }
+            if (views <= 9999) {
+                const k = Math.floor(views / 1000);
+                const remainder = Math.round(views % 1000 / 100);
+                return k + '.' + remainder + 'K'
+            }
+            if (views <= 999999) {
+                const k = Math.floor(views / 1000);
+                return k + 'K'
+            }
+        },
+        getDateDifference(date: string) {
+            const today = new Date().valueOf();
+            const dateTime = new Date(date).valueOf();
+            const diffTime = today - dateTime;
+            const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+            if (diffDays < 1) {
+                return '9 hours ago';
+            }
+            if (diffDays < 7) {
+                return diffDays - 1 + (diffDays === 1 ? ' day ago' : ' days ago');
+            }
+            if (diffDays < 31) {
+                return Math.floor(diffDays / 7) + (Math.floor(diffDays / 7) === 1 ? ' week ago' : ' weeks ago')
+            }
+            if (diffDays < 365) {
+                return Math.floor(diffDays / 31) + (Math.floor(diffDays / 31) === 1 ? ' month ago' : ' months ago')
+            }
+            if (diffDays >= 365) {
+                return Math.floor(diffDays / 365) + (Math.floor(diffDays / 365) === 1 ? ' year ago' : ' years ago')
+            }
+        }
+    },
+    beforeMount() {
+        this.showSidemenu = store.getters.showSidemenu
+        store.watch((getters) => getters.showSidemenu, () => {
+            this.showSidemenu = store.getters.showSidemenu;
+        })
+    },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+.videos {
+    margin-left: 240px;
+    padding: 20px;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    background: #181818;
+    .video {
+        flex: 1 1;
+        flex-basis: 23%;
+        margin: 1%;
+        max-width: 23%;
+        cursor: pointer;
+        .thumbnail {
+            position: relative;
+            img {
+                width: 100%;
+            }
+            .duration {
+                position: absolute;
+                bottom: 0;
+                right: 0;
+                margin: 4px 4px 10px;
+                background: rgba(0, 0, 0, 0.8);
+                font-size: 12px;
+                border-radius: 2px;
+                padding: 3px 4px;
+                font-weight: 500;
+            }
+        }
+        &-data {
+            display: flex;
+            position: relative;
+            .video-info {
+                display: flex;
+                width: 90%;
+                .video-channel-pp {
+                    width: 36px;
+                    height: 36px;
+                    border-radius: 50%;
+                    margin-top: 12px;
+                    margin-right: 12px;
+                }
+                .video-metadata {
+                    margin-top: 12px;
+                    span {
+                        display: inline-block;
+                    }
+                    .video-title {
+                        font-weight: 500;
+                        font-size: 16px;
+                        margin-bottom: 4px;
+                        line-height: 1.4rem;
+                        display: -webkit-box;
+                        -webkit-line-clamp: 2;
+                        -webkit-box-orient: vertical;
+                        overflow: hidden;
+                        text-overflow: ellipsis;
+                    }
+                    .video-channel-data {
+                        display: flex;
+                        .video-channel-name {
+                            font-weight: 400;
+                            font-size: 14px;
+                            color: #aaa;
+                            &:hover {
+                                color: #fff;
+                            }
+                        }
+                        svg {
+                            fill: #aaa;
+                            width: 14px;
+                            height: 14px;
+                            margin-left: 4px;
+                        }
+                    }
+                    .video-data {
+                        color: #aaa;
+                    }
+                    .video-statistics {
+                        color: #aaa;
+                        font-size: 14px;
+                        margin-top: 5px;
+                        .video-views::after {
+                            content: "•";
+                            margin: 0 4px;
+                        }
+                    }
+                }
+            }
+            .video-options {
+                position: absolute;
+                top: 0;
+                right: -10px;
+                visibility: hidden;
+                svg {
+                    height: 24px;
+                    margin-top: 12px;
+                }
+            }
+        }
+        &:hover .video-options {
+            visibility: visible;
+        }
+    }
+    &.fullPage {
+        margin-left: 72px;
+        .video {
+            flex-basis: 18%;
+            max-width: 18%;
+        }
+    }
+    @media screen and (max-width: 1330px) {
+        .video {
+            flex-basis: 31%;
+            max-width: 31%;
+        }
+        &.fullPage {
+            .video {
+                flex-basis: 23%;
+                max-width: 23%;
+            }
+        }
+    }
+    @media screen and (max-width: 884px) {
+        .video {
+            flex-basis: 48%;
+            max-width: 48%;
+        }
+        &.fullPage {
+            .video {
+                flex-basis: 31%;
+                max-width: 31%;
+            }
+        }
+    }
+    @media screen and (max-width: 658px) {
+        .video {
+            flex-basis: 98%;
+            max-width: 98%;
+        }
+        &.fullPage {
+            .video {
+                flex-basis: 48%;
+                max-width: 48%;
+            }
+        }
+    }
+}
+</style>
